34257553f5bb991c0b3c63abb080e60ef17519cc
rksumon51 <rk.sumon51@gmail.com>
2026-03-12 22:18:57 +0600

    Modify STREAM_KEY and implement playlist creation
    Updated the STREAM_KEY and added a function to create a playlist from .mp4 files.

--- live.py
+++ live.py
@@ -1,8 +1,17 @@
 import os
 
-STREAM_KEY = "YOUR_STREAM_KEY"
+STREAM_KEY = "z96x-91ph-1gmj-d9sx-1hy2"
+
+def create_playlist():
+    videos = [f for f in os.listdir() if f.endswith(".mp4")]
+
+    with open("playlist.txt", "w") as f:
+        for video in videos:
+            f.write(f"file '{video}'\n")
 
 while True:
+    create_playlist()
+
     cmd = f"ffmpeg -re -f concat -safe 0 -i playlist.txt -vf scale=720:1280 -c:v libx264 -preset veryfast -b:v 1800k -maxrate 1800k -bufsize 3600k -pix_fmt yuv420p -c:a aac -b:a 128k -f flv rtmp://a.rtmp.youtube.com/live2/{STREAM_KEY}"
-    
+
     os.system(cmd)

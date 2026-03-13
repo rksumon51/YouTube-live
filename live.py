8d8dbb20fb36739f0eb6e960e223c6d422b6557d
rksumon51 <rk.sumon51@gmail.com>
2026-03-13 12:16:59 +0600

    Refactor ffmpeg command for improved error handling

--- live.py
+++ live.py
@@ -14,6 +14,13 @@ def create_playlist():
 while True:
     create_playlist()
 
-    cmd = f"ffmpeg -re -f concat -safe 0 -i playlist.txt -vf scale=720:1280 -c:v libx264 -preset veryfast -b:v 1800k -maxrate 1800k -bufsize 3600k -pix_fmt yuv420p -c:a aac -b:a 128k -f flv rtmp://a.rtmp.youtube.com/live2/{STREAM_KEY}"
+    cmd = f"""ffmpeg -re -fflags +genpts -err_detect ignore_err \
+    -f concat -safe 0 -i playlist.txt \
+    -vf scale=720:1280 \
+    -vsync 1 -async 1 \
+    -c:v libx264 -preset veryfast -b:v 1800k -maxrate 1800k -bufsize 3600k \
+    -pix_fmt yuv420p \
+    -c:a aac -b:a 128k \
+    -f flv rtmp://a.rtmp.youtube.com/live2/{STREAM_KEY}"""
 
     os.system(cmd)
